13216f18548f0e0b0bf73f6d7c1700949708411d
Andrew Kyrios <1995ark@comcast.net>
2015-11-16 15:08:05 -0800

    adds JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,48 @@
+'use strict';
+
+
+var myApp = angular.module('myApp','myApp.directive', [])
+.controller('MyCtrl', ['$scope', '$http','$setValidity', function($scope, $http, $setValidity) { 
+    $scope.userInfo = {};
+    $scope.userInfo.birthdate = Date.parse(userSignUp.birthdate); 
+    $scope.success = false;
+    $scope.reset = function(userSignUp){
+    if(userSignUp.$setPristine){
+        userSignUp.$setPristine();
+    } else {
+        userSignUp.$pristine = true;
+        userSignUp.$dirty = false;
+        angular.forEach(userSignUp, function (input, key) {
+    if (input.$pristine)
+        input.$pristine = true;
+    if (input.$dirty) {
+        input.$dirty = false;
+            }
+        });
+    }
+};
+    $scope.signUp = function(userSignUp){
+    $scope.success = true;
+    alert('success');
+};
+.directive('pwCheck', [function () {
+                    return {
+                    require: 'ngModel',
+                    link: function (scope, elem, attrs, ctrl) {
+                    var firstPassword = '#' + attrs.pwCheck;
+                    elem.add(firstPassword).on('keyup', function () {
+                    scope.$apply(function () {
+                    var v = elem.val()===$(firstPassword).val();
+                    ctrl.$setValidity('pwmatch', v);
+                });
+            });
+        }
+    }
+}])
+//Initial app.js
+
+
+
+
+
 //Initial app.js
