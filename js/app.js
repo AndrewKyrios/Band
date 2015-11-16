db909c02eb033aa76f8ba72d3f2d27e9c6301797
Domenica Mata <dcm93@uw.edu>
2015-11-16 15:26:23 -0800

    Password validation was fixed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
+'use strict';
+var myApp = angular.module('myApp', [])
+  .controller('MyCtrl', ['$scope', '$http', function($scope, $http) { 
+  		$scope.userInfo = {};
+  		$scope.userInfo.birthdate = Date.parse(userSignUp.birthdate);  
+  		$scope.success = false;
+  		$scope.reset = function(userSignUp){
+  			if(userSignUp.$setPristine){
+        		userSignUp.$setPristine();
+    		} else {
+        		userSignUp.$pristine = true;
+        		userSignUp.$dirty = false;
+        		angular.forEach(userSignUp, function (input, key) {
+            if (input.$pristine)
+                input.$pristine = true;
+            if (input.$dirty) {
+                input.$dirty = false;
+            }
+        });
+    }
+};
+$scope.signUp = function(userSignUp){
+		$scope.success = true;
+		alert('success');
+	};
+
+}])
 //Initial app.js
